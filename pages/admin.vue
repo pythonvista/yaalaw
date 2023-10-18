dfdc9bc75f4ebd790cdc55bf0c423cf8c0aa1e93
pythonvista <emeterv18@gmail.com>
2023-10-18 03:20:45 +0100

    dss

---
+++ pages/admin.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="h-screen flex justify-center items-center">
+      <q-btn @click="Goto" :loading="loading" color="black ">Login as Admin</q-btn>
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      data:()=>({
+          url: "admin.yaalawconsult.com",
+          loading: false
+      }),
+      setup() {
+      definePageMeta({
+        layout: 'home',
+      });
+    },
+      methods:{
+          Goto(){
+              this.loading = true
+              location.replace(this.url, '_blank')
+          }
+      }
+  }
+  </script>
+  
+  <style>
+  
+  </style>
