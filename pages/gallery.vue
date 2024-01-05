d7981a190566167ee9c5e1b47e56aacf274b0e31
Emeter Victor <93406217+pythonvista@users.noreply.github.com>
2024-01-05 20:20:39 +0100

    dsds

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -5,6 +5,137 @@
     <h1 class="text-black mb-10 text-2xl font-bold underline ">Image Gallery</h1>
 
     <div class="grid xs:grid-cols-1 md:grid-cols-2 gap-2 lg:grid-cols-3">
+     <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/1WRHYPM.jpg" />
+      </a>
+
+<a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/DUOdw4y.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/wYeIIYN.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/MRDpFfw.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/Hf20GA1.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/hGN4idc.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/raUMK12.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/PCIy23N.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/4RuCCkV.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/L6jobTU.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/X2Ljiq4.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/0G9P96A.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/40wBMXF.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/Sis17jT.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/L35gDTX.jpg" />
+      </a>
+
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/EfFRrC4.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/353QrWZ.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/H8OXcG8.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/LGoqAnk.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/aPrwQ35.jpg" />
+      </a>
+      <a
+        href=""
+        data-lightbox="homePortfolio"
+      >
+        <img src="https://i.imgur.com/3FZ9xVg.jpg" />
+      </a>
+
+
+
       <a
         href=""
         data-lightbox="homePortfolio"
